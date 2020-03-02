7cd586a6c4bd68a3d7d765ed33508470589f738a
Maram <S538365@nwmissouri.edu>
2020-03-02 10:32:53 -0600

    function

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 
 
-function maxLength(){
+function sentenceMaxLength(){
     var maxLength = 250;
     var currentLength = document.getElementById("sentence").value.length;
     console.log(currentLength, "length")
+
+    var lengthLimit = maxLength - currentLength;
+
+    document.getElementById("limit").innerHTML = lengthLimit
     
 }
